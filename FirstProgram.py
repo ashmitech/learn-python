ff239c32d310ed41cc641c3ac80880f8781ec089
Mahesh Sharma <160108610+ashmitech@users.noreply.github.com>
2024-02-21 08:02:25 +0545

    Files uploaded
    Files of first program and guidelines from Aapna College uploaded

---
+++ FirstProgram.py
@@ -0,0 +1,15 @@
+#This is my first program
+"""Print string
+variable string
+variable float
+"""
+print ("Hello Mahesh")
+name="Ram"
+age=29
+a =-30.55
+b=10
+c=a/b
+d=a//b
+print (c,d)
+print (name)
+
